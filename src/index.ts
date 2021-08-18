6ad002b59d9d9b2826241290832f9babc637d1a1
asmsuechan <suenagaryoutaabc@gmail.com>
2021-08-18 22:54:08 +0900

    Remove console.log

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,3 @@ export const convertToHTMLString = (markdown: string) => {
   const htmlString = generate(asts);
   return htmlString;
 };
-
-// console.log(convertToHTMLString('1111111\n```\nabc\n<aaa></bbb>\n```\n\n* aaaa\n* bbb\n  * ccc\n* ddd'));
-// console.log(convertToHTMLString('|left|center|right|\n|:-|:-:|-:|\n|left|center|right|'));
-// console.log(convertToHTMLString('> quote\n>> quote'));
-// console.log(convertToHTMLString('* **bbbbb**'));
-// console.log(convertToHTMLString('# h1\n__aaaaa__'));
-// console.log(convertToHTMLString('[aaa](https://a.com)'));
-// console.log(convertToHTMLString('* a\n* b\n  * c\n* d'));
-// console.log(convertToHTMLString('* **bold**\n  * __nested__'));
-// console.log(convertToHTMLString('1. a'));
